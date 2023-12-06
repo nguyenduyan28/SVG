abf8ab80d064faf8e28216255b05fe0d8929dbaa
nguyenduyan28 <62472570+nguyenduyan28@users.noreply.github.com>
2023-12-06 23:25:14 +0700

    draw path

--- SVGDemo/stdafx.cpp
+++ SVGDemo/stdafx.cpp
@@ -3,142 +3,174 @@
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
 unsigned char opacity2alpha(double opacity) {
-	opacity = max(0.0, min(1.0, opacity));
-	return  static_cast<unsigned char>(opacity * 255.0);
+    opacity = max(0.0, min(1.0, opacity));
+    return  static_cast<unsigned char>(opacity * 255.0);
 }
 
 Shape::Shape() {
-	this->fill.r = this->fill.g = this->fill.b = 0;
-	this->stroke.r = this->stroke.g = this->stroke.b = 0;
-	this->fillOpacity = 1.0;
-	this->strokeOpacity = 1.5;
-	this->strokeWidth = 2;
+    this->fill.r = this->fill.g = this->fill.b = 0;
+    this->stroke.r = this->stroke.g = this->stroke.b = 255;
+    this->fillOpacity = 1.0;
+    this->strokeOpacity = 1.5;
+    this->strokeWidth = 2;
     this->hasStroke = false;
+    this->translate = Points({ 0.0, 0.0 });
+    this->rotate = 0;
+    this->scale = Points({ 0.0, 0.0 });
+}
+
+
+void Shape::drawShape() {
+
 }
 
 RectangleSVG::RectangleSVG() {
-	point.x = point.y = 0;
-	width = 0;
-	height = 0;
+    point.x = point.y = 0;
+    width = 0;
+    height = 0;
 }
 
 void RectangleSVG::drawRectangleSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
-
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	Pen pen(Color(strokealpha, stroke.r, stroke.b, stroke.g), strokeWidth);
-	Rect rect(point.x, point.y, width, height);
-	graphics.DrawRectangle(&pen, rect);
-	SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
-	graphics.FillRectangle(&brush, rect);
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    Pen pen(Color(strokealpha, stroke.r, stroke.b, stroke.g), strokeWidth);
+    Rect rect(point.x, point.y, width, height);
+    graphics.DrawRectangle(&pen, rect);
+    SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
+    graphics.FillRectangle(&brush, rect);
 }
 
 TextSVG::TextSVG() {
-	point.x = point.y = 0;
-	int fontSize = 12;
-	info = "";
+    point.x = point.y = 0;
+    int fontSize = 12;
+    info = "";
 }
 
 void TextSVG::drawTextSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	FontFamily  fontFamily(L"Times New Roman");
-	Font        font(&fontFamily, fontSize, FontStyleRegular, UnitPixel);
-	PointF      pointF(point.x - fontSize, point.y - fontSize);
-	SolidBrush  solidBrush(Color(fillalpha, fill.r, fill.g, fill.b));
-	wstring infostr = wstring(info.begin(), info.end());
-	const WCHAR* infocstr = infostr.c_str();
-	graphics.DrawString(infocstr, -1, &font, pointF, &solidBrush);
-    
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    FontFamily  fontFamily(L"Times New Roman");
+    Font        font(&fontFamily, fontSize, FontStyleRegular, UnitPixel);
+    PointF      pointF(point.x - fontSize, point.y - fontSize);
+
+    wstring infostr = wstring(info.begin(), info.end());
+    const WCHAR* infocstr = infostr.c_str();
+    GraphicsContainer container = graphics.BeginContainer();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    SolidBrush  solidBrush(Color(fillalpha, fill.r, fill.g, fill.b));
+    graphics.DrawString(infocstr, -1, &font, pointF, &solidBrush);
+    graphics.EndContainer(container);
 }
 
 LineSVG::LineSVG() {
-	from.x = from.y = to.x = to.y = 0;
+    from.x = from.y = to.x = to.y = 0;
 }
 
 void LineSVG::drawLineSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
-	graphics.DrawLine(&pen, from.x, from.y, to.x, to.y);
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    GraphicsContainer container = graphics.BeginContainer();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
+    graphics.DrawLine(&pen, from.x, from.y, to.x, to.y);
+    graphics.EndContainer(container);
 }
 
 CircleSVG::CircleSVG() {
-	point.x = point.y = 0;
-	radius = 0.0;
+    point.x = point.y = 0;
+    radius = 0.0;
 }
 
 void CircleSVG::drawCircleSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
-	Rect rect(point.x - radius, point.y - radius, radius * 2 , radius * 2);
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    GraphicsContainer container = graphics.BeginContainer();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
+    Rect rect(point.x - radius, point.y - radius, radius * 2, radius * 2);
     SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
     graphics.FillEllipse(&brush, rect);
-	graphics.DrawEllipse(&pen, rect);
-	
+    graphics.DrawEllipse(&pen, rect);
+    graphics.EndContainer(container);
 }
 
 PolylineSVG::PolylineSVG() {
-	points.clear();
+    points.clear();
 }
 
 void PolylineSVG::drawPolylineSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
-	vector <Point> newP;
-	for (int i = 0; i < points.size(); i++) {
-		newP.push_back(Point{ points[i].x, points[i].y });
-	}
-	Point* p = newP.data();
-	SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
-	graphics.FillPolygon(&brush, p, newP.size());
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
+    vector <PointF> newP;
+    for (int i = 0; i < points.size(); i++) {
+        newP.push_back(PointF{ points[i].x, points[i].y });
+    }
+    PointF* p = newP.data();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    GraphicsContainer container = graphics.BeginContainer();
+    SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
+    graphics.FillPolygon(&brush, p, newP.size());
     if (strokeWidth)
-	graphics.DrawLines(&pen, p, newP.size());
+        graphics.DrawLines(&pen, p, newP.size());
+    graphics.EndContainer(container);
 }
 
 EllipseSVG::EllipseSVG() {
-	c.x = c.y = 0;
-	radiusX = radiusY = 0.0;
+    c.x = c.y = 0;
+    radiusX = radiusY = 0.0;
 }
 
 
 void EllipseSVG::drawEllipseSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
-	Rect rect(c.x - radiusX, c.y - radiusY, radiusX * 2, radiusY * 2);
-	SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
-	graphics.FillEllipse(&brush, rect);
-	graphics.DrawEllipse(&pen, rect);
-	
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
+    Rect rect(c.x - radiusX, c.y - radiusY, radiusX * 2, radiusY * 2);
+    SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
+    GraphicsContainer container = graphics.BeginContainer();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    graphics.FillEllipse(&brush, rect);
+    graphics.DrawEllipse(&pen, rect);
+    graphics.EndContainer(container);
 }
 
 
 
-
 PolygonSVG::PolygonSVG() {
-	
-	points.clear();
+
+    points.clear();
 }
 
 
@@ -146,19 +178,23 @@ PolygonSVG::PolygonSVG() {
 void PolygonSVG::drawPolygonSVG(Graphics& graphics) {
     graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
     graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-	unsigned char fillalpha, strokealpha;
-	fillalpha = opacity2alpha(fillOpacity);
-	strokealpha = opacity2alpha(strokeOpacity);
-	Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
-	vector <Point> newP;
-	for (int i = 0; i < points.size(); i++) {
-		newP.push_back(Point{points[i].x, points[i].y });
-	}
-	Point* p = newP.data();
-	SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
-	graphics.FillPolygon(&brush, p, newP.size());
-	graphics.DrawPolygon(&pen, p, newP.size());
-	
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
+    vector <PointF> newP;
+    for (int i = 0; i < points.size(); i++) {
+        newP.push_back(PointF{ points[i].x, points[i].y });
+    }
+    PointF* p = newP.data();
+    GraphicsContainer container = graphics.BeginContainer();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
+    graphics.FillPolygon(&brush, p, newP.size());
+    graphics.DrawPolygon(&pen, p, newP.size());
+    graphics.EndContainer(container);
 }
 
 void setProperties(char* nodeName, vector<pair<string, string>> a, Graphics& graphics)
@@ -611,4 +647,107 @@ void PathSVG::setPath(vector<pair<string, string>>a)
         }
         else (*this).setShape(attributeName, attributeValue);
     }
+}
+PathSVG::PathSVG() {
+    startPoint.x = startPoint.y = curPoint.x = curPoint.y = 0;
+}
+
+void PathSVG::updatePoint(Points newPoint) {
+    this->curPoint = newPoint;
+}
+
+void PathSVG::drawPathSVG(Graphics& graphics) {
+    
+    unsigned char fillalpha, strokealpha;
+    fillalpha = opacity2alpha(fillOpacity);
+    strokealpha = opacity2alpha(strokeOpacity);
+    GraphicsContainer container = graphics.BeginContainer();
+    graphics.TranslateTransform(translate.x, translate.y);
+    graphics.ScaleTransform(scale.x, scale.y);
+    graphics.RotateTransform(rotate);
+    GraphicsPath myPath;
+    Pen pen(Color(strokealpha, stroke.r, stroke.g, stroke.b), strokeWidth);
+    SolidBrush brush(Color(fillalpha, fill.r, fill.g, fill.b));
+    for (int i = 0; i < dData.size(); i++) {
+        char nameCommand = dData[i].first;
+        vector <Points> data = dData[i].second;
+        if (i == 0) {
+            startPoint = data[0];
+            curPoint = data[0];
+            if (nameCommand == 'M' || nameCommand == 'm') {
+                if (data.size() > 1) {
+                    myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(data[0].x), int(data[0].y) });
+                    updatePoint(data[0]);
+                    for (int i = 1; i < data.size(); i++) {
+                        myPath.AddLine(Point{ int(data[i - 1].x), int(data[i - 1].y) }, Point{ int(data[i].x), int(data[i].y) });
+                        updatePoint(data[i]);
+                    }
+                }
+                else
+                    updatePoint(data[0]);
+            }
+        }
+        else {
+            if (nameCommand == 'M') {
+                if (data.size() > 1) {
+                    myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(data[0].x), int(data[0].y)});
+                    updatePoint(data[0]);
+                    for (int i = 1; i < data.size(); i++) {
+                        myPath.AddLine(Point{ int(data[i - 1].x), int(data[i - 1].y) }, Point{ int(data[i].x), int(data[i].y) });
+                        updatePoint(data[i]);
+                        
+                    }
+                    startPoint = data[data.size() - 1];
+                }
+                else {
+                    updatePoint(data[0]);
+                    startPoint = data[0];
+                }
+            }
+            else if (nameCommand == 'm') {
+                updatePoint(Points{ data[0].x + curPoint.x, data[0].x + curPoint.y });
+            }
+            else if (nameCommand == 'L') {
+                myPath.AddLine(Point{int(curPoint.x), int(curPoint.y) }, Point{ int(data[0].x), int(data[0].y) });
+                updatePoint(data[0]);
+            }
+            else if (nameCommand == 'l') {
+                myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(curPoint.x + data[0].x), int(curPoint.y + data[0].y) });
+                updatePoint(Points{ curPoint.x + data[0].x, curPoint.y + data[0].y });
+            }
+            else if (nameCommand == 'H') {
+                myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(data[0].x), int(curPoint.y) });
+                updatePoint(Points{ data[0].x, curPoint.y });
+            }
+            else if (nameCommand == 'h') {
+                myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(curPoint.x + data[0].x), int(curPoint.y) });
+                updatePoint(Points{ data[0].x + curPoint.x, curPoint.y });
+            }
+            else if (nameCommand == 'V') {
+                myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(curPoint.x), int(data[0].y) });
+                updatePoint(Points{ curPoint.x, data[0].y });
+            }
+            else if (nameCommand == 'v') {
+                myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(curPoint.x), int(data[0].y + curPoint.y) });
+                updatePoint(Points{ curPoint.x, data[0].y + curPoint.y });
+            }
+            else if (nameCommand == 'C') {
+                myPath.AddBezier(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(data[0].x), int(data[0].y) }, Point{ int(data[1].x), int(data[1].y) }, Point{ int(data[2].x), int(data[2].y) });
+                updatePoint(Points{ data[2].x, data[2].y });
+            }
+            else if (nameCommand == 'c') {
+                myPath.AddBezier(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(curPoint.x + data[0].x), int(curPoint.y + data[0].y) }, Point{ int(curPoint.x + data[1].x), int(curPoint.y + data[1].y) }, Point{ int(curPoint.x + data[2].x), int(curPoint.y + data[2].y) });
+                updatePoint(Points{ curPoint.x + data[2].x, curPoint.y + data[2].y });
+            }
+            else if (nameCommand == 'Z' || nameCommand == 'z') {
+                myPath.AddLine(Point{ int(curPoint.x), int(curPoint.y) }, Point{ int(startPoint.x), int(startPoint.y) });
+                updatePoint(startPoint);
+            }
+        }
+    }
+    graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
+    graphics.SetSmoothingMode(SmoothingModeAntiAlias);
+    graphics.DrawPath(&pen, &myPath);
+    graphics.FillPath(&brush, &myPath);
+    graphics.EndContainer(container);
 }
