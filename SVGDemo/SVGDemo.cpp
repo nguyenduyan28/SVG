d35b4d9dbf7b2b7e62867e4dda1855a767de377a
nguyenduyan28 <62472570+nguyenduyan28@users.noreply.github.com>
2023-12-14 20:10:37 +0700

    Update commandline argurment

--- SVGDemo/SVGDemo.cpp
+++ SVGDemo/SVGDemo.cpp
@@ -20,7 +20,7 @@ void applyAttributesToChildren(xml_node<>* parentNode, vector<pair<string, strin
     }
 }
 
-void read(xml_node<>* node, vector<pair<string, string>>& gAttributes, Graphics& graphics) 
+void read(xml_node<>* node, vector<pair<string, string>>& gAttributes, Graphics& graphics)
 {
     while (node) {
         char* nodeName = node->name();
@@ -64,12 +64,12 @@ void read(xml_node<>* node, vector<pair<string, string>>& gAttributes, Graphics&
     }
 }
 
-void DrawSVGFile(string& filename, HDC hdc) {
+void DrawSVGFile(wstring& filename, HDC hdc) {
     Graphics graphics(hdc);
 
     xml_document<> doc;
 
-    ifstream file("svg-15.svg");
+    ifstream file(filename);
     vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
     buffer.push_back('\0');
@@ -87,82 +87,94 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 {
-   HWND                hWnd;
-   MSG                 msg;
-   WNDCLASS            wndClass;
-   GdiplusStartupInput gdiplusStartupInput;
-   ULONG_PTR           gdiplusToken;
-   HDC                 hdc;
-   PAINTSTRUCT         ps;
+    HWND                hWnd;
+    MSG                 msg;
+    WNDCLASS            wndClass;
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR           gdiplusToken;
+    HDC                 hdc;
+    PAINTSTRUCT         ps;
 
 
-   
-   
-   // Initialize GDI+.
-   GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-   
-   wndClass.style          = CS_HREDRAW | CS_VREDRAW;
-   wndClass.lpfnWndProc    = WndProc;
-   wndClass.cbClsExtra     = 0;
-   wndClass.cbWndExtra     = 0;
-   wndClass.hInstance      = hInstance;
-   wndClass.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
-   wndClass.hCursor        = LoadCursor(NULL, IDC_ARROW);
-   wndClass.hbrBackground  = (HBRUSH)GetStockObject(WHITE_BRUSH);
-   wndClass.lpszMenuName   = NULL;
-   wndClass.lpszClassName  = TEXT("GettingStarted");
-   
-   RegisterClass(&wndClass);
-   
-   hWnd = CreateWindow(
-      TEXT("GettingStarted"),   // window class name
-      TEXT("SVG Demo"),  // window caption
-      WS_OVERLAPPEDWINDOW,      // window style
-      CW_USEDEFAULT,            // initial x position
-      CW_USEDEFAULT,            // initial y position
-      CW_USEDEFAULT,            // initial x size
-      CW_USEDEFAULT,            // initial y size
-      NULL,                     // parent window handle
-      NULL,                     // window menu handle
-      hInstance,                // program instance handle
-      NULL);                    // creation parameters
-      
-   ShowWindow(hWnd, iCmdShow);
-   UpdateWindow(hWnd);
-   
-   while(GetMessage(&msg, NULL, 0, 0))
-   {
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
-   }
-   
-   GdiplusShutdown(gdiplusToken);
-   return msg.wParam;
+
+
+    // Initialize GDI+.
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = WndProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = hInstance;
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = TEXT("GettingStarted");
+
+    RegisterClass(&wndClass);
+
+    hWnd = CreateWindow(
+        TEXT("GettingStarted"),   // window class name
+        TEXT("SVG Demo"),  // window caption
+        WS_OVERLAPPEDWINDOW,      // window style
+        CW_USEDEFAULT,            // initial x position
+        CW_USEDEFAULT,            // initial y position
+        CW_USEDEFAULT,            // initial x size
+        CW_USEDEFAULT,            // initial y size
+        NULL,                     // parent window handle
+        NULL,                     // window menu handle
+        hInstance,                // program instance handle
+        NULL);                    // creation parameters
+
+    ShowWindow(hWnd, iCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    GdiplusShutdown(gdiplusToken);
+    return msg.wParam;
 }  // WinMain
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, 
-   WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+    WPARAM wParam, LPARAM lParam)
 {
-   HDC          hdc;
-   PAINTSTRUCT  ps;
-   
-   switch(message)
-   {
-   case WM_PAINT: {
-       hdc = BeginPaint(hWnd, &ps);
-       Graphics graphics(hdc);
-       graphics.SetSmoothingMode(SmoothingModeAntiAlias);
-       string svgFilename = "sample.svg";
-       DrawSVGFile(svgFilename, hdc);
-       EndPaint(hWnd, &ps);
-       return 0;
-   }
-   case WM_DESTROY:
-      PostQuitMessage(0);
-      return 0;
-   default:
-      return DefWindowProc(hWnd, message, wParam, lParam);
-   }
+    HDC          hdc;
+    PAINTSTRUCT  ps;
+
+    switch (message)
+    {
+    case WM_PAINT: {
+        hdc = BeginPaint(hWnd, &ps);
+        Graphics graphics(hdc);
+        graphics.SetSmoothingMode(SmoothingModeAntiAlias);
+        int argc;
+        LPWSTR commandLine = GetCommandLineW();
+        LPWSTR* argv = CommandLineToArgvW(commandLine, &argc);
+        wstring svgFilename;
+
+        if (argc > 1) {
+            svgFilename = argv[1];
+        }
+        else {
+            // Default SVG file name if not provided in command line
+            svgFilename = L"sample.svg";
+        }
+
+        DrawSVGFile(svgFilename, hdc);
+        EndPaint(hWnd, &ps);
+        return 0;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
 } // WndProc
 
 ULONG_PTR gdiplusToken;
